28c22d4430592d00a5fad5ad38288563ee9b03f3
Guyon Moree <guyon.moree@gmail.com>
2013-09-19 17:12:01 +0200

    - bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tcc',
-    version='0.9.34',
+    version='0.9.35',
     description='Simple but effective comments app',
     #long_description=open('readme').read(),
     keywords='',
